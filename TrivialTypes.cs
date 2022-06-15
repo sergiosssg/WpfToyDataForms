52853e1b2fb04524c65dd816923080477abb78c7
sergiosssg <sergiosssg@gmail.com>
2022-06-15 20:56:48 +0300

    added   public class AccountPeriodTime .

---
+++ TrivialTypes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfToyDataForms
+{
+
+    public class AccountPeriodTime
+    {
+        public int Year
+        {
+            get;
+            set;
+        }
+        public int Month
+        {
+            get;
+            set;
+        }
+
+        public AccountPeriodTime()
+        {
+
+        }
+
+    }
+}
