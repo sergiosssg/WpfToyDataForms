2dd28ea14d10db778cc461930b02cc31f3d2170c
sergiosssg <sergiosssg@gmail.com>
2022-07-21 16:17:15 +0300

    in  class  TypeOfColumnValueForFieldFilter  added properties :     --  public string ColumnNAme     --  public string FieldNAme     --  public SortingDirection SortingDirection     --  public string NameOfType  --  defult constructor .

--- MechanismForFieldFilter.cs
+++ MechanismForFieldFilter.cs
@@ -6,9 +6,34 @@ using System.Threading.Tasks;
 
 namespace WpfToyDataForms
 {
+
+    public enum SortingDirection
+    {
+        Ascending, Descending
+    }
+
+
     public partial class TypeOfColumnValueForFieldFilter
     {
+        private string _columnName;
+        private string _fieldName;
 
+        
+
+
+        public string ColumnNAme  { get => this._columnName; set  { this._columnName = value; } }
+
+        public string FieldNAme { get => this._fieldName;   set { this._fieldName = value; } }
+
+        public SortingDirection SortingDirection { get; set; }
+
+        public string NameOfType { get; set; }
+
+
+        public TypeOfColumnValueForFieldFilter()
+        {
+            this.SortingDirection = SortingDirection.Ascending;
+        }
     }
 
     public partial class ColumnValueForFieldFilter
