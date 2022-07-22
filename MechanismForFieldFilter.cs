94cf7957d2cd11ba5fd34f47eac4e119844b78c8
sergiosssg <sergiosssg@gmail.com>
2022-07-22 15:14:36 +0300

    changed class  ColumnValueForFieldFilter,  made  it  parametrised :    --  ColumnValueForFieldFilter<T> .

--- MechanismForFieldFilter.cs
+++ MechanismForFieldFilter.cs
@@ -20,7 +20,7 @@ namespace WpfToyDataForms
 
         
 
-
+    
         public string ColumnNAme  { get => this._columnName; set  { this._columnName = value; } }
 
         public string FieldNAme { get => this._fieldName;   set { this._fieldName = value; } }
@@ -32,12 +32,24 @@ namespace WpfToyDataForms
 
         public TypeOfColumnValueForFieldFilter()
         {
+            this._columnName =  this._fieldName = "ID";
+            this.NameOfType = "string";
             this.SortingDirection = SortingDirection.Ascending;
         }
     }
 
-    public partial class ColumnValueForFieldFilter
+    public partial class ColumnValueForFieldFilter<T>
     {
+        private bool _IamEmpty;
+        private T? _value;
+        TypeOfColumnValueForFieldFilter _typeOfColumnValueForFieldFilter;
+
+        public ColumnValueForFieldFilter()
+        {
+            this._typeOfColumnValueForFieldFilter = new TypeOfColumnValueForFieldFilter();
+            this._IamEmpty = true;
+            this._value = default(T);
+        }
 
     }
 
