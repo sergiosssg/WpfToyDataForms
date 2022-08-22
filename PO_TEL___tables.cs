75607d186684783ba36143e8e79199377a6b3d82
sergiosssg <sergiosssg@gmail.com>
2022-08-22 13:09:34 +0300

    created  plain class  PO_TEL_MOB_SPR  without binding with data table of DB.

--- PO_TEL___tables.cs
+++ PO_TEL___tables.cs
@@ -125,6 +125,170 @@ namespace WpfToyDataForms
         }
     }
 
+    // TEL_MOB_SPR
+    public class PO_TEL_MOB_SPR
+    {
+        public int IDOfCellPhone
+        {   //  ID_MOB_TEL  integer
+            get;
+            set;
+        }
 
+        public string Phone
+        {  //   NOM_TEL  char(12)
+            get;
+            set;
+        }
+
+        public string Fio
+        {   //   FIO  varchar(100)
+            get;
+            set;
+        }
+
+        public string Tarif
+        {   //  TARIF  varchar(50)
+            get;
+            set;
+        }
+
+        public decimal Limit
+        {   //  LIMIT  double precision
+            get;
+            set;
+        }
+
+        public Boolean IsRoumingSet
+        {   //  PR_ROUMING  char(1)
+            get;
+            set;
+        }
+
+        public DateTime BeginOf
+        {   //  DATE_BEGIN  timestamp
+            get;
+            set;
+        }
+
+        public DateTime EndOf
+        {   //  DATE_END   timestamp
+            get;
+            set;
+        }
+
+        public int IDOperator
+        {  //  ID_OPERATOR  integer
+            get;
+            set;
+        }
+
+        public int IDDepartment
+        {   //  ID__PODR  integer
+            get;
+            set;
+        }
+
+        public string PhoneType
+        {   //  VID_TEL  char(1)
+            get;
+            set;
+        }
+
+        public DateTime WhenInserted
+        {   //  DATE_INS  timestamp
+            get;
+            set;
+        }
+
+        public DateTime WhenEdited
+        {   //  DATE_IZM  timestamp
+            get;
+            set;
+        }
+
+        public int IDWhoInserted
+        {   //  ID_USER_INS  integer
+            get;
+            set;
+        }
+
+        public int IDWhoEdited
+        {   //  ID_USER_IZM  integer
+            get;
+            set;
+        }
+
+        public string JobTitle
+        {   //  DOLGNOST  varchar(200)
+            get;
+            set;
+        }
+
+        public string NoteComment
+        {   //  PRIMECH  varchar(150)
+            get;
+            set;
+        }
+
+        public string PhoneTypeTogle
+        {   //  NAIM  char(1)
+            get;
+            set;
+        }
+
+        public string Nds
+        {   //  NDS  char(1)
+            get;
+            set;
+        }
+
+        public string Pf
+        {   //  PF  char(1)
+            get;
+            set;
+        }
+
+        public string TabelNumber
+        {   //  TAB_NOM  char(4)
+            get;
+            set;
+        }
+
+        public string FiredSign
+        {   //  PR_UVOL  char(1)
+            get;
+            set;
+        }
+
+        public string Mvz
+        {   //  CEX_MVZ  char(3)
+            get;
+            set;
+        }
+
+        public string Pr
+        {   //  PR  char(1)
+            get;
+            set;
+        }
+
+        public string CardNumber
+        {   //  NOM_KART  varchar(8)
+            get;
+            set;
+        }
+
+        public int Pernr
+        {   //  PERNR  integer
+            get;
+            set;
+        }
+
+        public DateTime Perem
+        {   //  DATE_PEREM  date
+            get;
+            set;
+        }
+    }
 
 }
