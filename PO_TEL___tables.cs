8a336cb30a547e15f0556fca9afec81b69a8f012
sergiosssg <sergiosssg@gmail.com>
2022-08-23 12:47:07 +0300

    fixed  PO_TEL_MOB_SPR.

--- PO_TEL___tables.cs
+++ PO_TEL___tables.cs
@@ -158,7 +158,7 @@ namespace WpfToyDataForms
             set;
         }
         [Column("LIMIT")]
-        public decimal? Limit
+        public double? Limit
         {   //  LIMIT  double precision
             get;
             set;
