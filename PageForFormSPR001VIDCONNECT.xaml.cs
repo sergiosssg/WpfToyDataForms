c5a7ea96c3f20860e63cd314780f948ed1d343c0
sergiosssg <sergiosssg@gmail.com>
2022-09-07 14:39:26 +0300

    in class  PageForFormSPR001VIDCONNECT  continue edditing  event handler _innerDataGrid_BeginningEdit .

--- PageForFormSPR001VIDCONNECT.xaml.cs
+++ PageForFormSPR001VIDCONNECT.xaml.cs
@@ -163,7 +163,19 @@ namespace WpfToyDataForms
         {
             PO_TEL_VID_CONNECT po_TEL_VID_CONNECT = getCurrentElementBeforeChangingFormDataGrid( sender);
             int? id = getCurrentIdNumber(  sender);
-            ;
+            if (po_TEL_VID_CONNECT.isIamEmpty())
+            {
+                ;
+            }
+            else if(this._ID_of_selectedRecord != null && this._ID_of_selectedRecord > 0 && this._pO_TEL_VID_CONNECT__selected != null && this._ID_of_selectedRecord != id)
+            {
+                this._pO_TEL_VID_CONNECT__selected = po_TEL_VID_CONNECT;
+                this._ID_of_selectedRecord = id;
+            }
+            else
+            {
+
+            }
             ;
             ;
             ;
