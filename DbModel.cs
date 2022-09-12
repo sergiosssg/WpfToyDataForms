b6803964ced0e2e13c861aaf10a3d9027a960217
sergiosssg <sergiosssg@gmail.com>
2022-09-12 11:33:51 +0300

    continue edditing class 'DbAppContext' method OnModelCreating  :  protected override void OnModelCreating(ModelBuilder modelBuilder)      -- modelBuilder.HasDefaultSchema("pnone"); .

--- DbModel.cs
+++ DbModel.cs
@@ -22,10 +22,10 @@ namespace WpfToyDataForms
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.HasDefaultSchema("pnone");
+            //modelBuilder.HasDefaultSchema("pnone");
             // modelBuilder.Entity<PO_TEL_OPERATOR>().HasOne(telefonOperator => telefonOperator.ParentIDConnect).WithMany(p => p.TelefonOperators).HasForeignKey(fk => fk.ParentIDConnect);
             //modelBuilder.Entity<PO_TEL_OPERATOR>().HasOne(telefonOperator => telefonOperator.ParentIDConnect).WithMany(p => p.TelefonOperators);
-            modelBuilder.Entity<PO_TEL_OPERATOR>().HasOne(p => p.ParentIDConnect).WithMany(p => p.TelefonOperators).HasForeignKey(p => p.IDConnect);
+            modelBuilder.HasDefaultSchema("pnone").Entity<PO_TEL_OPERATOR>().HasOne(p => p.ParentIDConnect).WithMany(p => p.TelefonOperators).HasForeignKey(p => p.IDConnect);
         }
 
 
