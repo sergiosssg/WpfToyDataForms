306ce1ea7130dbf9acc714aeae33cfab3f11fb9e
sergiosssg <sergiosssg@gmail.com>
2022-09-23 12:47:07 +0300

    .

--- DBStorageAccessories.cs
+++ DBStorageAccessories.cs
@@ -24,8 +24,8 @@ namespace WpfToyDataForms
         {
             DbConnectionStringBuilder builder = new SqlConnectionStringBuilder();
 
-            builder["Data Source"] = @"xplandev\DEVMAIN";         //  Office setting string for development  MS SQL DB in interprise server
-            //builder["Data Source"] = "localhost";               //  Office setting string for local MS SQL DB
+            //builder["Data Source"] = @"xplandev\DEVMAIN";         //  Office setting string for development  MS SQL DB in interprise server
+            builder["Data Source"] = "localhost";               //  Office setting string for local MS SQL DB
             //builder["Data Source"] = @"localhost\SQLExpress";   //  Home setting string for local MS SQL DB
 
 
