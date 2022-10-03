35964a6b533fa3b71992bf7abefa85c535169739
sergiosssg <sergiosssg@gmail.com>
2022-10-03 09:40:25 +0300

    changed  DB connection string in : 'DataBaseFacilities'. 'DbAppContext', so that it points to remote DB on 'DEVMAIN' SQL server.

--- DBStorageAccessories.cs
+++ DBStorageAccessories.cs
@@ -24,13 +24,13 @@ namespace WpfToyDataForms
         {
             DbConnectionStringBuilder builder = new SqlConnectionStringBuilder();
 
-            //builder["Data Source"] = @"xplandev\DEVMAIN";         //  Office setting string for development  MS SQL DB in interprise server
-            builder["Data Source"] = "localhost";               //  Office setting string for local MS SQL DB
+            builder["Data Source"] = @"xplandev\DEVMAIN";         //  Office setting string for development  MS SQL DB in interprise server
+            //builder["Data Source"] = "localhost";               //  Office setting string for local MS SQL DB
             //builder["Data Source"] = @"localhost\SQLExpress";   //  Home setting string for local MS SQL DB
 
 
-            //builder["Database"] = "SM";                            //  Office setting string for development  MS SQL DB in interprise server
-            builder["Database"] = "sampd_cexs";                  //  Office and Home setting string for local MS SQL DB
+            builder["Database"] = "SM";                            //  Office setting string for development  MS SQL DB in interprise server
+            //builder["Database"] = "sampd_cexs";                  //  Office and Home setting string for local MS SQL DB
 
             builder["integrated Security"] = "true";
 
